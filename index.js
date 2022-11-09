c21d2bd024212c5a0dbe5a789f4ad281fa1eecfb
prandeveloper <pranaydevelopersveltosest@gmail.com>
2022-11-09 14:41:41 +0530

    main branch copy

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ import 'react-native-gesture-handler';
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-import RNPreventScreenshot from 'react-native-screenshot-prevent';
+// import RNPreventScreenshot from 'react-native-screenshot-prevent';
 
 export default function index() {
-  RNPreventScreenshot.enabled(true);
+  // RNPreventScreenshot.enabled(true);
 
   return <App />;
 }
